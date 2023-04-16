252dee5b0f366d22ce349aa68bf4ab3ed74dad54
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-04-16 16:00:01 -0500

    Define the Entity type

--- src/render.ts
+++ src/render.ts
@@ -1,8 +1,16 @@
-export const RenderEntity = (entity: Entity) => {
-    const {type, width, height, x, y} = entity
-    const {ctx} = RenderContext
-    ctx.fillStyle = 'red'
-    ctx.fillRect(x, y, width, height)
-}
+type Entity = {
+    type: string;
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+};
 
-export default {RenderEntity}
+export const RenderEntity = (entity: Entity) => {
+    const { type, width, height, x, y } = entity;
+    const { ctx } = RenderContext;
+    ctx.fillStyle = "red";
+    ctx.fillRect(x, y, width, height);
+};
+
+export default { RenderEntity };
