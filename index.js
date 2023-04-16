9d299b55daca4d8734fa3a6a9a708b6015d37784
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-04-16 15:59:15 -0500

    Move the index file

--- index.js
+++
@@ -1,8 +0,0 @@
-import './assets/style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from '/assets/vite.svg'
-import { setupScore } from './src/ui/score'
-import {PlayerEntity} from './src/entities/player'
-import {RenderEntity} from './src/render'
-
-setupScore(document.querySelector('#score'))
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './assets/style.css'
+import { setupScore } from './ui/score'
+import { PlayerEntity } from './src/entities/player'
+import { RenderEntity } from './render'
+
+setupScore(document.querySelector('#score'))
