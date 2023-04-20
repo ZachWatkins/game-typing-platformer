170249ba43daa1e5a0cd024a4b3ae60351c9a5da
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-04-20 15:12:42 -0500

    Add custom global typedef

--- .d.ts
+++ .d.ts
@@ -1,20 +1,50 @@
 // Type definitions for [game-typing-platformer]
+// @see https://www.typescriptlang.org/docs/handbook/declaration-files/templates/global-d-ts.html
 // Project: [app]
 // Definitions by: [Zachary K. Watkins] <[https://zachwatkins.dev]>
 
-export namespace App {
-    export type Entity = {
-        type: string,
-        width: number,
-        height: number,
-        x: number,
-        y: number,
-    };
-    export type PlayerInput = {
-        up: boolean,
-        down: boolean,
-        left: boolean,
-        right: boolean,
-        jump: boolean,
-    };
+declare interface Point {
+    x: number,
+    y: number,
+}
+
+declare interface Line {
+    top: number,
+    left: number,
+    right: number,
+}
+
+declare interface Size {
+    width: number,
+    height: number,
+}
+
+declare interface Rect extends Line {
+    bottom: number,
+}
+
+declare interface Collider extends Rect {
+    collides: true,
+}
+
+declare interface Platform extends Line {
+    collides: true,
+}
+
+declare interface Entity extends Point, Collider, Size {
+    type: string,
+    collides: false,
+}
+
+declare type PlayerInput = {
+    up: boolean,
+    down: boolean,
+    left: boolean,
+    right: boolean,
+    jump: boolean,
+}
+
+declare interface RenderContext {
+    querySelector: (selector: string) => Node | null,
+    createElement: (tag: string) => Node,
 }
