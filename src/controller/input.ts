18b8dc5de9efd70f203af7c10dacfdd94d072c25
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-04-23 13:35:27 -0500

    Simplify the module by moving type definitions to the function

--- src/controller/input.ts
+++ src/controller/input.ts
@@ -1,38 +1,33 @@
 /**
- * This is an event listener class which listens for a subset of event codes from a source and calls a function when the subset changes.
+ * This is an event listener component which listens for a subset of event codes from a source and calls a function when the subset changes.
  *
  * @author Zachary K. Watkins
  */
-type InputEvent = { code: string }
 type HasEventListeners = {
-    addEventListener: (type: string, listener: (event: InputEvent) => void) => void,
-    removeEventListener: (type: string, listener: (event: InputEvent) => void) => void,
-}
-type Dispatcher = (code: string, state: boolean) => void
-type ObservedCodes = {
-    [code: string]: boolean,
+    addEventListener: (type: string, listener: (event: { code: string }) => void) => void,
+    removeEventListener: (type: string, listener: (event: { code: string }) => void) => void,
 }
 
 export function InputController(opts: {
     source: HasEventListeners,
-    dispatch: Dispatcher,
-    on: string,
-    off: string,
     codes: string[],
+    onEvent: string,
+    offEvent: string,
+    dispatch: (code: string, state: boolean) => void,
 }) {
-    const state = opts.codes.reduce((acc, code) => {
-        acc[code] = false
-        return acc
-    }, {} as ObservedCodes)
+    const state: { [code: string]: boolean } = {}
+    for (let i = opts.codes.length - 1; i >= 0; i--) {
+        state[opts.codes[i]] = false
+    }
 
-    opts.source.addEventListener(opts.on, (event: InputEvent): void => {
+    opts.source.addEventListener(opts.onEvent, (event: { code: string }): void => {
         if (state[event.code] === false) {
             state[event.code] = true
             opts.dispatch(event.code, true)
         }
     })
 
-    opts.source.addEventListener(opts.off, (event: InputEvent): void => {
+    opts.source.addEventListener(opts.offEvent, (event: { code: string }): void => {
         if (state[event.code] === true) {
             state[event.code] = false
             opts.dispatch(event.code, false)
