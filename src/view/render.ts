d55109a919f25af0ed3677d45a26711bc81807fd
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-05-08 21:19:32 -0500

    Refine canvas rendering

--- src/view/render.ts
+++ src/view/render.ts
@@ -7,14 +7,13 @@ import { renderStats } from '../common/stats'
 import { Player } from '../model/entity'
 import { falling } from '../controller/entity'
 import { updateStats } from '../common/stats'
-import { context } from './'
-import { clear } from './canvas'
+import { clear, paint } from './canvas'
 
 falling.start(Player)
 
 export default function render(): void {
     clear()
     updateStats(Player)
-    context.paint(Player)
+    paint(Player)
     renderStats()
 }
