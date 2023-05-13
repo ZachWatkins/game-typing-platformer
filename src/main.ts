07626b93824e51bfabd878e5564422a48b505a3c
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-05-12 22:25:48 -0500

    Add link to repository to the build for github pages

--- src/main.ts
+++ src/main.ts
@@ -20,3 +20,19 @@ container.appendChild(
 
 // Give useful feedback during development.
 container.appendChild(Stats())
+
+// Add information for GitHub Pages.
+const header = document.createElement('div')
+header.id = 'header'
+header.style.position = 'absolute'
+header.style.top = '0'
+header.style.left = '0'
+header.style.backgroundColor = '#000'
+header.style.color = '#FFF'
+header.style.boxShadow = '0 0 10px 0 rgba(0, 0, 0, 0.5)'
+header.style.padding = '10px'
+header.style.fontFamily = 'monospace'
+header.style.fontSize = '12px'
+header.style.zIndex = '1000'
+header.innerHTML = 'JavaScript Canvas game prototype (<a href="https://github.com/zachwatkins/game-typing-platformer">source code</a>).'
+container.appendChild(header)
